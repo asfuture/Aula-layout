e8bfc7ac72017c273fdc3421af990b78f0f4b9dc
Alex Ferreira <73206526+asfuture@users.noreply.github.com>
2021-05-04 11:18:12 -0300

    novo projeto

---
+++ index.php
@@ -0,0 +1,146 @@
+
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="css/style.css">
+
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    
+    
+    <title>Bloomin</title>
+</head>
+<body>
+    <header>
+    <nav class="nav-menu">
+
+      <div>
+          <ul class="menu-link">
+            <li>
+              <a href="#"><i class="fas fa-bars"></i></a>
+              </li>
+            <li>
+              <a href="#" >CLIENTS</a>
+            </li>
+            <li>
+              <a href="#">NEWS</a>
+            </li>
+            <li>
+              <a href="#" class="mitalent">MITALENT</a>
+            </li>
+          </ul>
+        </div>
+
+      <div>
+          <form class="form-pesuisa">
+            <input class="pesquisa" type="search">
+            <button class="btn-pesquisa" type="submit"><i class="fas fa-search"></i></button>
+          </form>
+      </div>
+</nav>
+
+<div class="div-vazia">
+</div>
+
+      <div class="div-foto-capa">
+        
+        <div class="titulo-georgina">
+          <h2>Georgina</h2>
+          <h2>
+            Alson <i class="fas fa-minus"></i></span>
+         </h2>
+          <p>YOUNG MODEL 2020</p>
+        </div>
+
+        <div class="grid">
+          <div class="grid-container">
+            <div class="grid-item item1">VIEW PROFILE</div>
+            <div class="grid-item item2"><i class="fas fa-play"></i></div>
+          </div>
+        </div>
+
+      </div>
+    </header>
+    
+    <section class="escopo">
+
+      <div>
+        <nav class="menu-nav-escopo">
+          <ul class="menu-ul-escopo">
+            <li>ACTOR</li>
+            <li>MUSICAN</li>
+            <li>COMEDIAN</li>
+            <li>MODEL</li>
+          </ul>
+        </nav>
+      </div>
+
+      <div class="flex">
+
+          <div>
+            <img src="img/foto.jpg">
+          </div>
+
+          <div>
+            <img src="img/foto1.jpg">
+          </div>
+
+          <div>
+            <img src="img/foto6.jpg">
+        
+          </div>
+
+          <div>
+            <img src="img/foto33.jpg">
+        
+          </div>
+          <div>
+            <img src="img/foto4.jpg">
+        
+          </div>
+          <div>
+            <img src="img/foto5.jpg">
+          
+          </div>
+          <div>
+            <img src="img/foto2.jpg">
+         
+          </div>
+          <div>
+            <img src="img/foto7.jpg">
+         
+          </div>
+    	</div>
+
+      <div class="news">
+        
+        <button>EXPLORE MORE</button>
+
+        <h1>Latest News </h1>
+        <br>
+      </div>
+
+      <footer>
+        <div class="div-footer">
+          <label for="" class="footer-mitalent">MITALENT</label>
+          <P>SIGN UP OUR NEWSLETTER <span><i class="fas fa-mail-bulk"></i></span></P>
+        </div>
+        <div class="div-footer1">
+          <label for="">copy@ MI Talent. Designed by Tranmautramm for Mas Impressions </label>
+          <P class="p-footer">
+            <span><i class="fab fa-facebook-f"></i></span>
+            <span><i class="fab fa-instagram"></i></span>
+            <span><i class="fab fa-youtube"></i></span>
+            <span><i class="fab fa-twitter"></i></span>
+          </P>
+        </div>
+      </footer>
+
+
+    
+    </section>
+</body>
+</html>
+
